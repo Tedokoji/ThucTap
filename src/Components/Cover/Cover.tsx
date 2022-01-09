d2c69e9c8ef489e7b55f4cf87e4982fbc41244de
unknown <tedokojikagami@gmail.com>
2022-01-09 23:18:41 +0700

    test

---
+++ src/Components/Cover/Cover.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Cover.scss'
+import PC from '../static file/PC.jpg'
+import logo from '../static file/Logo.jpg'
+function Cover() {
+    return (
+        <div className="cover">
+           <div className="cover-context-left">
+                <div className="LMS">
+                        <h1 className="LMStext">
+                            LMS - Learning<br/> Management System
+                        </h1>
+                        <button
+                        className="E-finance"
+                        >E-Finance</button>
+                </div>
+                <p>
+                    Hệ thống giáo dục được phát triển bởi
+                </p>
+                <img className="logo" src={logo} alt='logo'/>
+           </div>
+           <div className="cover-context-right">
+                <img className="pc" src={PC} alt='pc'/>
+            <p>
+                Designed by UI/UX Team of Alta Media
+            </p>
+           </div>
+        </div>
+    )
+}
+
+export default Cover
